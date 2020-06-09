b8d20fbf3a339b66b51ea80762bd453727909da8
Gaspare Joubert <gasparejoubert@gascosolutions.com>
2020-06-09 18:03:05 +0100

    FindTrendProfitPointFxSpeculator
    Add class FindTrendProfitPointFxSpeculator
    
    index.php
    Minor update
    Add require_once ('classes/FindTrendProfitPointFxSpeculator.php')
    Add $findTrendProfitPointFxSpeculator

---
+++ classes/FindTrendProfitPointFxSpeculator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class FindTrendProfitPointFxSpeculator
+ *
+ * Use the rate used to purchase a currency
+ * Calculate the Profit Point for this transaction
+ * Must factor in minimum profit percentage
+ * Must factor in cost of converting currency
+ * Must output information to a text file
+ */
+class FindTrendProfitPointFxSpeculator
+{
+    public function __construct()
+    {
+        $this->main();
+    }
+
+    private function main()
+    {
+
+    }
+}
